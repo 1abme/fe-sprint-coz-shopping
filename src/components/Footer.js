b4d1f70c59f75b6f4af04a851acf72d84ce795fe
kimhyejoo <rnrmfzl2659@gmail.com>
2023-05-16 12:21:55 +0900

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,21 @@
 import React from "react";
-function Footer() {
+import styled from "styled-components";
+
+const TagFooter = styled.footer`
+text-align: center;
+display: flex;
+flex-direction: column;
+font-family: Inter;
+font-size: 12px;
+font-weight: 400;
+color: #888888;
+`
+const Footer = () => {
   return (
-    <div>
-    </div>
+    <TagFooter>
+      <span>개인정보 처리방침 | 이용 약관</span>
+      <span>All rights reserved @ Codestates</span>
+    </TagFooter>
   )
 }
 
